207dba89f2f80ce5b21bb6441662ba57fe6dcca2
roshith-balendran <roshithbalendran@gmail.com>
2018-07-18 14:45:24 +0530

    Initial Commit.
    Added Demo Project Repository to GitHub.

---
+++ Whatsapp-Demo/Whatsapp-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whatsapp-Demo
+//
+//  Created by Roshith Balendran on 18/07/18.
+//  Copyright © 2018 Roshith Balendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
