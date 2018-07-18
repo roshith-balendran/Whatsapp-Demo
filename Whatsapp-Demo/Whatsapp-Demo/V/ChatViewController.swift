3f825e27a868707dc1d15231832cc7297f955283
Roshith Balendran <roshithbalendran@gmail.com>
2018-07-18 15:42:56 +0530

    Created Home and Chat ViewControllers

---
+++ Whatsapp-Demo/Whatsapp-Demo/V/ChatViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChatViewController.swift
+//  Whatsapp-Demo
+//
+//  Created by Roshith Balendran on 18/07/18.
+//  Copyright © 2018 Roshith Balendran. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- Whatsapp-Demo/Whatsapp-Demo/V/HomeViewController.swift
+++ Whatsapp-Demo/Whatsapp-Demo/V/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Whatsapp-Demo
 //
 //  Created by Roshith Balendran on 18/07/18.
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
